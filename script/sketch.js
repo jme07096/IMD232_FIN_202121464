172c53e6c0a04939a4c3f3973beda6b210b4cbfd
jme07096 <143993256+jme07096@users.noreply.github.com.>
2023-12-18 14:10:57 +0900

    ff

--- script/sketch.js
+++ script/sketch.js
@@ -1,16 +1,16 @@
-var x = 0;
-var y = 0;
-var stepSize = 9.0;
+let x = 0;
+let y = 0;
+let stepSize = 9.0;
 
-var font = 'Diphylleia'; // 폰트 변수 추가
+let font = 'Diphylleia'; // 폰트 변수 추가
 
-var letters =
+let letters =
   '♱ 안녕하세요!! ♱,마법사 여러분! 당신을 ✦✦ 마법의 할로윈파티에 초대합니다! ✦✦ 신비로운 마법과함께 많은 유령,괴물 친구들이 당신을 기다리고 있어요! 공포스럽고 재미있는 마법의 세계로 당신을 초대합니다!준비가 되었다면, 이곳으로 오세요! 당신이 오길 바라며, - 마법사 ';
-var fontSizeMin = 30;
-var angleDistortion = 0.0;
+let fontSizeMin = 30;
+let angleDistortion = 0.0;
 
-var counter = 0;
-var backgImg; // 이미지 변수 추가
+let counter = 0;
+let backgImg; // 이미지 변수 추가
 
 function preload() {
   backgImg = loadImage('./script/편지.png');
@@ -32,13 +32,13 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed && mouseButton == LEFT) {
-    var d = dist(x, y, mouseX, mouseY);
+    let d = dist(x, y, mouseX, mouseY);
     textSize(fontSizeMin + d / 2);
-    var newLetter = letters.charAt(counter);
+    let newLetter = letters.charAt(counter);
     stepSize = textWidth(newLetter);
 
     if (d > stepSize) {
-      var angle = atan2(mouseY - y, mouseX - x);
+      let angle = atan2(mouseY - y, mouseX - x);
 
       push();
       translate(x, y);
@@ -56,11 +56,11 @@ function draw() {
 }
 
 function drawBackground() {
-  var imgWidth = windowWidth * 0.8; // 이미지 너비를 윈도우의 80%로 설정
-  var imgHeight = (backgImg.height * imgWidth) / backgImg.width; // 이미지 비율에 맞게 높이 계산
+  let imgWidth = windowWidth * 0.8; // 이미지 너비를 윈도우의 80%로 설정
+  let imgHeight = (backgImg.height * imgWidth) / backgImg.width; // 이미지 비율에 맞게 높이 계산
 
-  var posX = (windowWidth - imgWidth) / 2; // 가로 중앙 정렬을 위한 X 위치 계산
-  var posY = (windowHeight - imgHeight) / 2; // 세로 중앙 정렬을 위한 Y 위치 계산
+  let posX = (windowWidth - imgWidth) / 2; // 가로 중앙 정렬을 위한 X 위치 계산
+  let posY = (windowHeight - imgHeight) / 2; // 세로 중앙 정렬을 위한 Y 위치 계산
 
   image(backgImg, posX, posY, imgWidth, imgHeight); // 이미지 그리기
 }
